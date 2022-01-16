38cc3687b6594cafb34459345305c17488b42776
Chelsea Elrod <celrod0724@gmail.com>
2022-01-16 13:01:11 -0600

    no real significant changes

--- levelup.session.sql
+++ levelup.session.sql
@@ -11,3 +11,15 @@ SELECT * FROM levelupapi_game;
 SELECT * FROM levelupapi_gamer;
 SELECT * FROM levelupapi_event e JOIN levelupapi_game g
 ON e.game_id = g.id WHERE g.id=1
+
+SELECT
+    title,
+    maker,
+    number_of_players,
+    skill_level,
+    gamer_id,
+    gametype_id
+FROM
+    levelupapi_game
+WHERE
+    id = 3
